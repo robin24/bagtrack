2bfba919537a3a2ee84eb02cde5deb441c991220
Robin Kipp <robin@robin-kipp.net>
2017-09-27 22:47:17 +0200

    • Implement custom accessibility action to enable / disable tracking • Hide on-screen "enable tracking" label and switch from VO for easier navigation

--- BagTrack/View Controllers/BagCell.swift
+++ BagTrack/View Controllers/BagCell.swift
@@ -19,6 +19,7 @@ class BagCell: UITableViewCell {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var proximityLabel: UILabel!
     @IBOutlet weak var trackingSwitch: UISwitch!
+    @IBOutlet weak var trackingLabel:UILabel!
     weak var delegate:BagCellDelegate?
     var bag:Bag! {
         didSet {
@@ -30,7 +31,6 @@ class BagCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -42,9 +42,30 @@ class BagCell: UITableViewCell {
         nameLabel.text = bag.name
         proximityLabel.text = bag.proximityForDisplay()
         trackingSwitch.isOn = bag.isTrackingEnabled
+        trackingSwitch.isAccessibilityElement = false
+        trackingLabel.isAccessibilityElement = false
+        if trackingSwitch.isOn {
+            let action = UIAccessibilityCustomAction(name: "Disable Tracking", target: self, selector: #selector(toggleSwitchAccessibilityAction(_:)))
+            self.accessibilityCustomActions = [action]
+        } else {
+            let action = UIAccessibilityCustomAction(name: "Enable Tracking", target: self, selector: #selector(toggleSwitchAccessibilityAction(_:)))
+            self.accessibilityCustomActions = [action]
+        }
+
     }
-    @IBAction func onTrackingSwitchToggled(_ sender: UISwitch) {
+    @IBAction func onTrackingSwitchToggled(_ sender: Any) {
         delegate?.bagCell(self, didToggleTrackingFor: bag)
+    }
+    @objc func toggleSwitchAccessibilityAction(_ sender:Any) {
+        trackingSwitch.isOn = !trackingSwitch.isOn
+        delegate?.bagCell(self, didToggleTrackingFor: bag)
+        if trackingSwitch.isOn {
+            let action = UIAccessibilityCustomAction(name: "Disable Tracking", target: self, selector: #selector(toggleSwitchAccessibilityAction(_:)))
+            self.accessibilityCustomActions = [action]
+        } else {
+            let action = UIAccessibilityCustomAction(name: "Enable Tracking", target: self, selector: #selector(toggleSwitchAccessibilityAction(_:)))
+            self.accessibilityCustomActions = [action]
+        }
     }
 
 }
